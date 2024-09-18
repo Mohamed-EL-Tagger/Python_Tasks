32dc718b37cb427285d9eddf6c4ffc9135d3a1bf
Mohammed_El_Tagger <maltager658@gmail.com>
2024-09-18 12:25:43 +0300

    Commit Changes

---
+++ clac.py
@@ -0,0 +1,10 @@
+# All Operations For Two Values
+first_value = int(input("Enter first value: "))
+Last_value = int(input("Enter last value: "))
+
+print(first_value + Last_value)
+print(first_value - Last_value)
+print(first_value * Last_value)
+print(first_value / Last_value)
+print(first_value ^ Last_value)
+
---
+++ task1.py
@@ -0,0 +1,20 @@
+# Field
+
+# Name = input('What is your Name')
+# Age = input('What is your Age')
+# Educational_stage = input('What is your Educational_stage')  # KG(1,2,3) ,, Primary ,, Prep ,, Secondary
+# School = input('What is your School Name ?')
+# Graduation = input('Are You Graduated From Your School Yes/NO')
+# Hoppy = input('What is Your Favourite Hoppy')
+# # information Output
+#
+# print(f'ABOUT {Name}')
+# age = input('What is your Age')
+# gender = input('What is your Gender He/She')
+# educational_stage = input('What is your Educational_stage')  # KG(1,2,3) ,, Primary ,, Prep ,, Secondary
+# school = input('What is your School Name ?')
+# graduation = input('Are You Graduated From Your School Yes/NO')
+# location = input('What is Your Location')
+# hoppy = input('What is Your Favourite Hoppy')
+# print(f' {name} is {age} years old , {gender} is in {school} {educational_stage} School in {location} , {name} likes {hoppy} ')
+
